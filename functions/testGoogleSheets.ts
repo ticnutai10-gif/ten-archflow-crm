8aba3963ac0841d191747b00dc033cdfdf7c157b
יעקב טננבוים <jj1212t@gmail.com>
2025-12-29 15:03:52 +0000

    File changes

--- functions/testGoogleSheets.ts
+++ functions/testGoogleSheets.ts
@@ -1,5 +1,5 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
-import { google } from 'npm:googleapis';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { google } from 'npm:googleapis@137.1.0';
 
 Deno.serve(async (req) => {
   const results = {
